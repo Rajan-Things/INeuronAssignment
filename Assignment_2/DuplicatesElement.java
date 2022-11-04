df8a609d92d452ff6e917f103b02dfe3baa3840b
Rajan-Things <rajan.githubthings@gmail.com>
2022-11-04 16:22:21 +0530

    Done

---
+++ Assignment_2/DuplicatesElement.java
@@ -0,0 +1,38 @@
+package Assignment_2;
+
+import java.util.*;
+
+public class DuplicatesElement {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();		
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for(int i=0; i<n; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		
+		int[] t = new int[max+1];
+		
+		for(int i=0; i<n; i++) {
+			t[arr[i]] += 1;
+		}
+		
+		for(int i=0; i<t.length; i++) {
+			if(t[i] > 1) {
+				System.out.println(i);
+			}
+		}
+		sc.close();
+	}
+
+}
